1eaa5caa6780ba2e0fb2c3d33882266d3f7f5ead
Dorian DE ROSA <dorian.de-rosa@epita.fr>
2021-11-17 22:00:41 +0100

    s

--- LevelK/Brute.php
+++ LevelK/Brute.php
@@ -21,7 +21,31 @@ class Brute
      */
     public function force()
     {
-
+        
         // @TODO
+        $set = 'abcdefghijklmnopqrstuvwxyz';
+
+        $string = 'aaaa';
+        print($this->hash);
+        print("\n");
+
+        for ($i=0; $i < 26; $i++) { 
+            for ($j=0; $j < 26; $j++) {
+                for ($k=0; $k < 26; $k++) { 
+                    for ($l=0; $l < 26; $l++) { 
+                        $string[0] = $set[$i];
+                        $string[1] = $set[$j];
+                        $string[2] = $set[$k];
+                        $string[3] = $set[$l];
+                        if ($this->hash == md5($string)) {
+                            $this->origin = $string;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        $this->origin = $string;
     }
 }
